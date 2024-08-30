06e3b0658235d4f2d0e65a088e3402b68ceda1dd
Md. Toufiqul Islam Tanmoy <67703225+toufiqulislamtanmoy@users.noreply.github.com>
2024-08-30 12:03:02 +0600

    iinitial project setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,6 @@
+interface NavbarProps {}
+const Navbar = () => {
+  return <div>This is the navbar</div>;
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: '#A8D5BA',   // Soft Mint Green
+        secondary: '#E6E3F1', // Light Lavender
+        accent: '#FF6F61',    // Coral Pink
+        darkPrimary: '#2C3E50',  // Darker primary for dark mode
+        darkSecondary: '#34495E', // Darker secondary for dark mode
+        darkAccent: '#E74C3C',    // Darker accent for dark mode
+      },
+
     },
   },
   plugins: [],
